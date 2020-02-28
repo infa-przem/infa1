79ae0ccc7617d2afd1e57c9d44699ac6d9fd2149
infa-przem <61532440+infa-przem@users.noreply.github.com>
2020-02-28 12:29:37 +0100

    Update najmniejsza liczba z posrod n liczb.cpp

--- najmniejsza liczba z posrod n liczb.cpp
+++ najmniejsza liczba z posrod n liczb.cpp
@@ -18,6 +18,7 @@ int oblicz(int x, int y)
 {
     for(int i=1; i<x; i++)
     {
+        cout << "Podaj " << i+1 << "liczbe: " << endl;
         z=Dane();
         if(z<y)
         {
@@ -32,7 +33,9 @@ int oblicz(int x, int y)
 
 int mainn()
 {
+    cout << "Ile liczb? ";
     n = Dane();
+    cout << "Podaj 1 liczbe: " << endl;
     a = Dane();
 
     cout << "Najmniesza to: " << oblicz(n,a);
